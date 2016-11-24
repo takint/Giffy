9efad3e4abe5c511897dfbbb5e1c7ccc871b33ec
Squall Taki <taki.lnt@gmail.com>
2016-11-24 22:15:26 +0700

    - Giffy publish web.

--- Giffy.Entities/Models/Image.cs
+++ Giffy.Entities/Models/Image.cs
@@ -5,9 +5,10 @@ namespace Giffy.Entities.Models
         public int Id { get; set; }
         public string Name  { get; set; }
         public string Description { get; set; }
-		public string Url { get; set; }
+        public string Url { get; set; }
+        public string AlternativeUrl { get; set; }
         public string CropInfo { get; set; }
-        public bool IsActived { get; set; }       
+        public bool IsActived { get; set; }
         public ImageType ImageType { get; set; }
 
         public int? PostId { get; set; }
